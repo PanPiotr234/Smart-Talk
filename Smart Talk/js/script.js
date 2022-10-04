23c0817449ecfb7d8576ce2616efb352dba52580
kubagp <37304664+kubagp1@users.noreply.github.com>
2022-10-04 12:24:08 +0000

    Fix dragging, refactor responisiveness

--- Smart Talk/js/script.js
+++ Smart Talk/js/script.js
@@ -1,68 +1,78 @@
 // auto-writing
 
 var typed = new Typed(".auto-type", {
-    strings: ["Facebooku", "Instagramie", "Linkedinie"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: false,
-})
+  strings: ["Facebooku", "Instagramie", "Linkedinie"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: false,
+});
 
 // swiper
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 40,
-    autoplay: {
-      delay: 6500,
-      disableOnInteraction: false,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
+  },
+  autoplay: {
+    delay: 6500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
-var swiper = new Swiper(".yourSwiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    autoplay: {
-      delay: 500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
+// var swiper = new Swiper(".yourSwiper", {
+//     slidesPerView: 2,
+//     spaceBetween: 30,
+//     autoplay: {
+//       delay: 500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+// });
 
-var swiper = new Swiper(".mineSwiper", {
-    slidesPerView: 1,
-    autoplay: {
-      delay: 5500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
+// var swiper = new Swiper(".mineSwiper", {
+//     slidesPerView: 1,
+//     autoplay: {
+//       delay: 5500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+// });
 
 // fotter-waves
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry)
+    console.log(entry);
     if (entry.isIntersecting) {
-      entry.target.classList.add('show');
+      entry.target.classList.add("show");
     } else {
-      entry.target.classList.remove('show');
+      entry.target.classList.remove("show");
     }
   });
 });
 
-// pojawianie się elementów 
+// pojawianie się elementów
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
 // FAQ section
@@ -74,19 +84,19 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 var len = acc.length;
 for (i = 0; i < len; i++) {
-  try{
-  acc[i].querySelector(".arrow").textContent = ARROW_COLLAPSED
+  try {
+    acc[i].querySelector(".arrow").textContent = ARROW_COLLAPSED;
   } catch {}
 
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
-      panel.style.maxHeight = null; 
-      this.querySelector(".arrow").textContent = ARROW_COLLAPSED
+      panel.style.maxHeight = null;
+      this.querySelector(".arrow").textContent = ARROW_COLLAPSED;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-      this.querySelector(".arrow").textContent = ARROW_EXPANDED
+      this.querySelector(".arrow").textContent = ARROW_EXPANDED;
     }
   });
 }
